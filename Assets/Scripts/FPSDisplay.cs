5cb1331dfd6411975c0ee83612801ad594a22a2a
Unknown <jziaty@avans.nl>
2018-11-17 10:34:52 +0100

    Starting on puzzle 2

---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FPSDisplay : MonoBehaviour
+{
+    public int avgFrameRate;
+    public TextMeshProUGUI display_Text;
+
+    public void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = avgFrameRate.ToString() + " FPS";
+    }
+}
